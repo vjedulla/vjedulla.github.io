a75b911faaddcfe06717e991f7d30c6490b96c6f
vjedulla <armando.alliu@gmail.com>
2022-06-21 17:09:38 +0200

    added doodle

--- assets/js/main.voronoi.js
+++ assets/js/main.voronoi.js
@@ -3,7 +3,6 @@ const box = document.getElementById('canvas-container');
 let width = box.clientWidth;
 let height = box.clientHeight;
 
-
 function sketch(p) {
     p.init_voronoi = function(){
         var voronoi = new Voronoi();
@@ -14,35 +13,59 @@ function sketch(p) {
             return  parseInt(a + Math.floor(Math.random() * (b+1)));   
         }
 
+        var method = randomNumber(0, 1); // which method will be executed (sampling)
+        const n = randomNumber(20, 50); // how many points
+        const offsetX = width / 2; //randomNumber(-width*0.25, width * 0.25);
+        const offsetY = height / 2; // randomNumber(-height * 0.25, height * 0.25);
+        const zoomFactor = randomNumber(3, 6);
 
-        var n = randomNumber(14, 30);
         var points = [];
         for (let j = 0; j < n; j++) {
             const w = randomNumber(0, width);
             const h = randomNumber(0, height);
             
-            points.push({x: w, y: h});
+            if(method == 0){
+                points.push({x: (w * Math.cos(w)) / zoomFactor  + offsetX, y: (h * Math.sin(h)) / zoomFactor + offsetY});
+            }else if(method == 1){
+                points.push({x: w, y: h});
+            }
+            
         }
 
         var diagram = voronoi.compute(points, bbox);
 
-        return [points, diagram];
+        return [points, diagram, method];
     }
 
 
     p.draw_voronoi = function(vector){
         var points = vector[0];
         var diagram = vector[1];
+        var method = vector[2];
 
-        p.fill(217, 63, 81);
-        p.noStroke();
-        for (let index = 0; index < points.length; index++) {
-            const element = points[index];
-            
-            p.circle(element.x, element.y, 3);
+        var col_points = null;
+        var col_lines = null;
+
+        if(method == 1){
+            col_points = p.color(217, 63, 81);
+            col_lines = p.color(0, 109, 161);
+        }else if(method == 0){
+            col_points = p.color(0, 109, 161);
+            col_lines = p.color(217, 63, 81);
         }
 
-        p.stroke(0, 109, 161);
+        if(method == 1){
+            p.fill(col_points);
+            p.noStroke();
+            for (let index = 0; index < points.length; index++) {
+                const element = points[index];
+                
+                p.circle(element.x, element.y, 3);
+            }
+        }
+        
+
+        p.stroke(col_lines);
         for (let i = 0; i < diagram.edges.length; i++) {
             const e = diagram.edges[i];
         
@@ -58,12 +81,10 @@ function sketch(p) {
 
     p.setup = function () {
         vector = p.init_voronoi();
-        p.createCanvas(width,200);
+        p.createCanvas(width, 400);
 
         p.draw_voronoi(vector);
     }
-
-    var rect = [0, 0, 0]
   
     p.draw = function () {
         // console.log(p.deltaTime)
