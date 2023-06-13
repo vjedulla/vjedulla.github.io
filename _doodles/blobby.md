4fb89594bb1f2e53d96636ea1305809b35ed8454
vjedulla <armando.alliu@gmail.com>
2023-06-13 12:44:06 +0200

    created the blob doodle

---
+++ _doodles/blobby.md
@@ -0,0 +1,9 @@
+---
+layout: doodle
+title: Blobby
+subtitle: loud circles.
+js: "/assets/js/doodles/classes/blob.js"
+date: 13-06-2023
+---
+
+<div id="specific-doodle-container"></div>
--- assets/js/doodles/classes/blob.js
+++ assets/js/doodles/classes/blob.js
@@ -1,6 +1,8 @@
 class Blob extends Doodle{
     constructor() {
         super();
+        this.xphase = 0;
+        this.yphase = 0;
     }
 
     create_point(){
@@ -21,70 +23,90 @@ class Blob extends Doodle{
         return this;
     }
 
-    animate(){
-        clear();
-        let length = 400;
-        let randomvalues = []
+    blob(transx, transy, radius, color, bloboffset){
+        let noiseMax = 2;
+        let scale = 0.001;
 
+        let subdivCount = 1000;
 
-        let scale = 0.05
-        let zscale = 0.05
+        let subdiv = TWO_PI / subdivCount;
 
-        for(let i = 0; i < sqrt(length); i++){
-            randomvalues.push(noise(0 * scale, i * scale, millis() * scale * zscale))
-            // console.log(0, i)
-        }
-
-        for(let i = 1; i < sqrt(length)-1; i++){
-            randomvalues.push(noise(i * scale, sqrt(length)-1 * scale, millis() * scale* zscale))
-            // console.log(i, sqrt(length)-1)
-        }
-
-        for(let i = sqrt(length)-1; i > 0; i--){
-            randomvalues.push(noise(sqrt(length)-1 * scale, i * scale, millis() * scale* zscale))
-            // console.log(sqrt(length)-1, i)
-        }
-
-        for(let i = sqrt(length)-1; i > -1; i--){
-            randomvalues.push(noise(i * scale, 0 * scale, millis() * scale * zscale))
-            // console.log(i, 0)
-        }
-
-        // console.log(randomvalues);
-
-        let subdiv = TWO_PI / randomvalues.length;
-        let radius = 200;
-
-        strokeWeight(5)
+        strokeWeight(0)
         stroke(0);
 
         push();
-            translate(width / 2, height / 2)
-
-            let rad = 0;
+            translate(transx, transy)
 
             beginShape();
-            fill(0, noise(millis() * 0.001) * 110, noise(millis() * 0.001) * 160)
-                for(let j = 0; j < randomvalues.length + 1; j++){
-                    rad += subdiv;
-                    let x = radius * randomvalues[j % randomvalues.length] * cos(rad);
-                    let y = radius * randomvalues[j % randomvalues.length] * sin(rad);
-                    // let rnd = noise(x * scale, y * scale, millis() * scale * zscale)
-                    // x *= rnd
-                    // y *= rnd
-                    vertex(x, y);
-                    // console.log(j, x, y)
+            // fill(0, noise(millis() * 0.001) * color, noise(millis() * 0.001) * 160, 120)
+
+            fill(color.red, color.green, color.blue, color.alpha)
+
+            for(let a = 0, i = 0; a < TWO_PI; a+= subdiv, i++){
+                let xoff = map(cos(a + this.xphase + bloboffset), -1, 1, 0, noiseMax)
+                let yoff = map(sin(a + this.yphase + bloboffset), -1, 1, 0, noiseMax)
+
+                let r = map(noise(xoff, yoff, millis() * scale), 0, 1, radius, radius * 2)
+
+                let x = r * cos(a);
+                let y = r * sin(a);
+
+                if(i % 50 == 0){
+                    ellipse(x, y, 7, 7)
                 }
-            endShape();
+
+                vertex(x, y);
+            }
+            endShape(CLOSE);
         pop();
 
 
-        // noLoop();
+        this.xphase += PI/2000;
+        this.yphase += PI/1000;
+    }
+
+    animate(){
+        clear();
+        let blobOff = map(noise(frameCount) * 0.01, 0, 1, 35, 50)
+        
+        color = {
+            red: 0, 
+            green: map(noise(millis() * 0.001), 0, 1, 180, 220),
+            blue: map(noise(millis() * 0.001), 0, 1, 60, 170),
+            alpha: 120
+        }
+        this.blob(width / 2 -  width / blobOff, height / 2, 100, color, 0.2);
+
+        color = {
+            red: 0, 
+            green: map(noise(millis() * 0.001), 0, 1, 50, 100),
+            blue: map(noise(millis() * 0.001), 0, 1, 100, 160),
+            alpha: 120
+        }
+        this.blob(width / 2, height / 2, 100, color, 0);
+
+        color = {
+            red: 0, 
+            green: map(noise(millis() * 0.001), 0, 1, 0, 50),
+            blue: map(noise(millis() * 0.001), 0, 1, 30, 160),
+            alpha: 120
+        }
+        this.blob(width / 2, height / 2 + height / blobOff, 100, color, 1);
+
+
+        color = {
+            red: map(noise(millis() * 0.001), 0, 1, 50, 160), 
+            green: map(noise(millis() * 0.001), 0, 1, 0, 20),
+            blue: map(noise(millis() * 0.001), 0, 1, 0, 30),
+            alpha: 120
+        }
+        this.blob(width / 2 - width / blobOff - 20, height / 2 + height / blobOff + 20, 100, color, 0.5);
+
         return this;
     }
 
     static getName(){
-        return "blob"
+        return "blobs"
     }
  }
 
